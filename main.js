fc0b09725b6f2c27022865a9ca9601a19da4a455
Snorri967 <84525118+Snorri967@users.noreply.github.com>
2023-01-13 19:18:17 +0000

    Add winCheck function to compare inputs

--- main.js
+++ main.js
@@ -46,4 +46,22 @@ let playerChoice = function () {
   }
 }
 
+let winCheck = function (computerChoice, playerChoice) {
+  if (computerChoice === "Rock" && playerChoice === "Paper") {
+    alert(`${playerChoice} beats ${computerChoice} you win this round!`);
+  } else if (computerChoice === "Rock" && playerChoice === "Scissors") {
+    alert(`${computerChoice} beats ${playerChoice} you lose this round!`);
+  } else if (computerChoice === "Paper" && playerChoice === "Scissors") {
+    alert(`${playerChoice} beats ${computerChoice} you win this round!`);
+  } else if (computerChoice === "Paper" && playerChoice === "Rock") {
+    alert(`${computerChoice} beats ${playerChoice} you lose this round!`);
+  } else if (computerChoice === "Scissors" && playerChoice === "Rock") {
+    alert(`${playerChoice} beats ${computerChoice} you win this round!`);
+  } else if (computerChoice === "Scissors" && playerChoice === "Paper") {
+    alert(`${computerChoice} beats ${playerChoice} you lose this round!`);
+  } else {
+    alert(`You chose ${playerChoice} and the computer chose ${computerChoice} the round was a draw.`);
+  }
+}
 
+winCheck(computerChoice(), playerChoice());
