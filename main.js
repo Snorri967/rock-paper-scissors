92311066d1c33466417ee457fadf15a376657385
Snorri967 <84525118+Snorri967@users.noreply.github.com>
2023-01-18 23:00:13 +0000

    Update game to play via click and add CSS

--- main.js
+++ main.js
@@ -1,22 +1,66 @@
-function welcomeMessage() {
-  let confirmation = confirm("Welcome to Rock, Paper, Scissors");
-  if (confirmation) {
-    return;
-  } else {
-    goodbye();
-    errorKillScript();
-  }
+const container = document.createElement("div");
+container.classList.add("container");
+document.body.appendChild(container);
+
+const playerChoiceDiv = document.createElement("div");
+playerChoiceDiv.classList.add("playerChoiceDiv");
+container.appendChild(playerChoiceDiv);
+
+const resultDiv = document.createElement("div");
+resultDiv.classList.add("resultDiv");
+container.appendChild(resultDiv);
+
+const rockButton = document.createElement("button");
+rockButton.classList.add("rockButton");
+rockButton.textContent = "Rock";
+playerChoiceDiv.appendChild(rockButton);
+
+const paperButton = document.createElement("button");
+paperButton.classList.add("paperButton");
+paperButton.textContent = "Paper";
+playerChoiceDiv.appendChild(paperButton);
+
+const scissorsButton = document.createElement("button");
+scissorsButton.classList.add("scissorsButton");
+scissorsButton.textContent = "Scissors";
+playerChoiceDiv.appendChild(scissorsButton);
+
+const allRPSButtons = document.querySelectorAll(".playerChoiceDiv button");
+
+const roundResultPara = document.createElement("p");
+roundResultPara.classList.add("roundResult");
+
+const scorePara = document.createElement("p");
+scorePara.classList.add("score");
+
+const finalResultPara = document.createElement("p");
+finalResultPara.classList.add("finalResult");
+
+const playAgainButton = document.createElement("button");
+playAgainButton.classList.add("playAgainButton");
+playAgainButton.textContent = "Play Again";
+ 
+const welcomeDiv = document.createElement("div");
+welcomeDiv.classList.add("welcomeDiv");
+document.body.appendChild(welcomeDiv);
+
+function welcome() {
+  let welcomePara = document.createElement("p");
+  welcomePara.classList.add("welcomePara");
+  welcomePara.textContent = "Welcome to Rock, Paper, Scissors click the button to play!";
+  welcomeDiv.appendChild(welcomePara);
+  let welcomeButton = document.createElement("button");
+  welcomeButton.classList.add("welcomeButton");
+  welcomeButton.textContent = "Play";
+  welcomeDiv.appendChild(welcomeButton);
+  welcomeButton.addEventListener("click", () => {
+    welcomeDiv.style.display = "none";
+    container.style.display = "flex";
+    playGame();
+  })
 }
 
-function goodbye() {
-    alert("I guess you don't want to play, another time maybe");
-}
-
-function errorKillScript () {
-  throw new Error("This isn't an error, just exiting Javascript");
-}
-
-let computerChoice = function () {
+let getComputerChoice = function () {
   let getComputerChoice = Math.floor(Math.random() * 3) + 1;
   if (getComputerChoice === 1) {
     getComputerChoice = "Rock";
@@ -28,77 +72,75 @@ let computerChoice = function () {
   return getComputerChoice;
 }
 
-let playerChoice = function () {
-  let getPlayerChoice = prompt("Choose either 'Rock', 'Paper', or 'Scissors'");
-  if (getPlayerChoice === null) {
-    goodbye();
-    errorKillScript();
-  }
-  getPlayerChoice = `${getPlayerChoice.charAt(0).toUpperCase()}${getPlayerChoice.substring(1).toLowerCase()}`;
-  if (getPlayerChoice === "Rock" || getPlayerChoice === "Paper" || getPlayerChoice === "Scissors") {
-    return getPlayerChoice;
-  } else if (getPlayerChoice === "") {
-    alert("Please enter a valid name");
-    return playerChoice();
-  } else {
-    alert("Please enter a valid name");
-    return playerChoice();
-  }
+function playGame() {
+  let playerScore = 0;
+  let computerScore = 0;
+  allRPSButtons.forEach(button => {
+    button.addEventListener("click", (event) => {
+      const playerChoice = event.target.textContent;
+      let checkRound = winCheck(getComputerChoice(), playerChoice, playerScore, computerScore);
+      if (checkRound === 0) {
+        playerScore++;
+        scorePara.textContent = `Your score: ${playerScore} vs Computer score: ${computerScore}`;
+        resultDiv.appendChild(scorePara);
+      } else if (checkRound === 1) {
+        computerScore++;
+        scorePara.textContent = `You score: ${playerScore} vs Computer score: ${computerScore}`
+        resultDiv.appendChild(scorePara);
+      }
+      if (playerScore === 5) {
+        playerChoiceDiv.style.display = "none";
+        roundResultPara.style.display = "none";
+        scorePara.style.display = "none";
+        finalResultPara.textContent = `Congratulations you win!`;
+        resultDiv.appendChild(finalResultPara);
+        playAgain();
+      } else if (computerScore === 5) {
+        playerChoiceDiv.style.display = "none";
+        roundResultPara.style.display = "none";
+        scorePara.style.display = "none";
+        finalResultPara.textContent = `Unlucky you lost better luck next time!`;
+        resultDiv.appendChild(finalResultPara);
+        playAgain();
+      }
+    });
+  });
 }
 
 function winCheck(computerChoice, playerChoice) {
   if (computerChoice === "Rock" && playerChoice === "Paper") {
-    alert(`${playerChoice} beats ${computerChoice} you win this round!`);
-    return 0;
+    roundResultPara.textContent = `${playerChoice} beats ${computerChoice} you win this round!`;
+    resultDiv.appendChild(roundResultPara);
+    return 0
   } else if (computerChoice === "Rock" && playerChoice === "Scissors") {
-    alert(`${computerChoice} beats ${playerChoice} you lose this round!`);
-    return 1;
+    roundResultPara.textContent = `${computerChoice} beats ${playerChoice} you lose this round!`;
+    resultDiv.appendChild(roundResultPara);
+    return 1
   } else if (computerChoice === "Paper" && playerChoice === "Scissors") {
-    alert(`${playerChoice} beats ${computerChoice} you win this round!`);
-    return 0;
+    roundResultPara.textContent = `${playerChoice} beats ${computerChoice} you win this round!`;
+    resultDiv.appendChild(roundResultPara);
+    return 0
   } else if (computerChoice === "Paper" && playerChoice === "Rock") {
-    alert(`${computerChoice} beats ${playerChoice} you lose this round!`);
-    return 1;
+    roundResultPara.textContent = `${computerChoice} beats ${playerChoice} you lose this round!`;
+    resultDiv.appendChild(roundResultPara);
+    return 1
   } else if (computerChoice === "Scissors" && playerChoice === "Rock") {
-    alert(`${playerChoice} beats ${computerChoice} you win this round!`);
-    return 0;
+    roundResultPara.textContent = `${playerChoice} beats ${computerChoice} you win this round!`;
+    resultDiv.appendChild(roundResultPara);
+    return 0
   } else if (computerChoice === "Scissors" && playerChoice === "Paper") {
-    alert(`${computerChoice} beats ${playerChoice} you lose this round!`);
-    return 1;
+    roundResultPara.textContent = `${computerChoice} beats ${playerChoice} you lose this round!`;
+    resultDiv.appendChild(roundResultPara);
+    return 1
   } else {
-    alert(`You chose ${playerChoice} and the computer chose ${computerChoice} the round was a draw.`);
+    roundResultPara.textContent = `You chose ${playerChoice} and the computer chose ${computerChoice} the round was a draw.`;
+    resultDiv.appendChild(roundResultPara);
   }
 }
 
 function playAgain() {
-  let playAgain = confirm("Do you want to play another game enter?");
-  if (playAgain) {
-    location.reload();
-  } else {
-    goodbye();
-    errorKillScript();    
-  }
+  container.appendChild(playAgainButton);
+  playAgainButton.addEventListener("click", () => location.reload());
 }
 
-function playGame() {
-  let playerCounter = 0;
-  let computerCounter = 0;
-  for (let i = 0; i < 5; i++) {
-    let check = winCheck(playerChoice(), computerChoice());
-    if (check === 0) {
-      playerCounter++;
-    } else if (check === 1) {
-      computerCounter++;    
-    }
-  }
-  if (playerCounter > computerCounter) {
-    alert(`Congratulations you win with a score of ${playerCounter} to ${computerCounter}`)
-  } else if (computerCounter > playerCounter) {
-    alert(`Unlucky you lost with a score of ${playerCounter} to ${computerCounter}`)
-  } else {
-    alert("The game ended in a draw.")
-  }
-  playAgain();
-};
-
-playGame();
+welcome();
